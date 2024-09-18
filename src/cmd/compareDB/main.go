d7e519eb93eafe00a59bb54d052121c50ae6a01d
jenisepl <jenisepl@student.21-school.ru>
2024-09-18 12:49:30 +0300

    replace code in main for compareDB to func ReadDB

--- src/cmd/compareDB/main.go
+++ src/cmd/compareDB/main.go
@@ -1,33 +1,21 @@
 // Программа, которая читаем в формате JSON и возвращает их в XML и наоборот.
 package main
 
+import (
+	"day01/internal/dbreader"
+	"day01/pkg/utils"
+)
+
 func main() {
-	// Получаем путь к файлу из аргументов командной строки
-	//filepathForOld, filepathForNew, err := utils2.ParseFileForCompareDB()
-	//utils2.HandleError(err)
-	//
-	//// Создаем ридер на основе типа файла (JSON или XML)
-	//reader, _, err := dbreader.NewDBReader(filepathForOld)
-	//utils2.HandleError(err)
-	//
-	//// Читаем содержимое файла
-	//dataOld, err := utils2.ReadFile(filepathForOld)
-	//utils2.HandleError(err)
-	//
-	//// Десериализуем данные в структуру
-	//recipeOld, err := reader.Read(dataOld)
-	//utils2.HandleError(err)
-	//
-	//// Создаем ридер на основе типа файла (JSON или XML)
-	//reader, _, err = dbreader.NewDBReader(filepathForNew)
-	//utils2.HandleError(err)
-	//
-	//// Читаем содержимое файла
-	//dataNew, err := utils2.ReadFile(filepathForNew)
-	//utils2.HandleError(err)
-	//
-	//// Десериализуем данные в структуру
-	//recipeNew, err := reader.Read(dataNew)
-	//utils2.HandleError(err)
+	//Получаем путь к файлам из аргументов командной строки
+	filepathForOld, filepathForNew, err := utils.ParseFileForCompareDB()
+	utils.HandleError(err)
+
+	// Читаем файлы, десериализуем в структуры
+	recipeOld, _, err := dbreader.ReadDB(filepathForOld)
+	utils.HandleError(err)
+
+	recipeNew, _, err := dbreader.ReadDB(filepathForNew)
+	utils.HandleError(err)
 
 }
