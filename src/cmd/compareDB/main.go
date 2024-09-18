4a071ecb12b95c2708d312183ab5f2826d89e408
jenisepl <jenisepl@student.21-school.ru>
2024-09-18 14:10:18 +0300

    add change in main / compare struct

--- src/cmd/compareDB/main.go
+++ src/cmd/compareDB/main.go
@@ -4,6 +4,7 @@ package main
 import (
 	"day01/internal/dbreader"
 	"day01/pkg/utils"
+	"fmt"
 )
 
 func main() {
@@ -18,4 +19,51 @@ func main() {
 	recipeNew, _, err := dbreader.ReadDB(filepathForNew)
 	utils.HandleError(err)
 
+	// mapCakesOld - мапа для названия торта и структуры Recipe
+	mapCakesOld := make(map[string]dbreader.Recipe)
+
+	// ключ, значение nameCake - содержит структуру Recipe
+	for _, structRecipe := range recipeOld.Cakes {
+		mapCakesOld[structRecipe.Name] = structRecipe
+	}
+
+	mapCakesNew := make(map[string]dbreader.Recipe)
+
+	for _, structRecipe := range recipeNew.Cakes {
+		mapCakesNew[structRecipe.Name] = structRecipe
+	}
+
+	for nameOld, cakeOld := range mapCakesOld {
+		if cakeNew, exists := mapCakesNew[nameOld]; !exists {
+			fmt.Printf("REMOVED cake %v\n", nameOld)
+		} else {
+			if cakeOld.Time != cakeNew.Time {
+				fmt.Printf("CHANGED cooking time for cake %v - %v instead of %v\n", nameOld, cakeNew.Time, cakeOld.Time)
+			}
+
+			mapIngredientsOld := make(map[string]dbreader.Ingredient)
+			for _, ingredientOld := range cakeOld.Ingredients {
+				mapIngredientsOld[ingredientOld.Name] = ingredientOld
+			}
+
+			mapIngredientsNew := make(map[string]dbreader.Ingredient)
+			for _, ingredientNew := range cakeOld.Ingredients {
+				mapIngredientsNew[ingredientNew.Name] = ingredientNew
+			}
+
+			for ingrNameOld, _ := range mapIngredientsOld {
+				if _, exists := mapIngredientsNew[ingrNameOld]; !exists {
+					fmt.Printf("REMOVED ingredient %v for cake %v\n", ingrNameOld, nameOld)
+				}
+			}
+
+		}
+
+	}
+
+	for nameNew, _ := range mapCakesNew {
+		if _, exists := mapCakesOld[nameNew]; !exists {
+			fmt.Printf("ADDED cake %v\n", nameNew)
+		}
+	}
 }
