b14a1730fcc9522e4ea785b6b7bd946410df7df0
jenisepl <jenisepl@student.21-school.ru>
2024-09-18 17:03:58 +0300

    add map for compare  struct ingridints

--- src/cmd/compareDB/main.go
+++ src/cmd/compareDB/main.go
@@ -19,41 +19,46 @@ func main() {
 	recipeNew, _, err := dbreader.ReadDB(filepathForNew)
 	utils.HandleError(err)
 
-	// mapCakesOld - мапа для названия торта и структуры Recipe
+	// mapCakesOld - мапа ключ - название торта / значение - структура Recipe
 	mapCakesOld := make(map[string]dbreader.Recipe)
-
-	// ключ, значение nameCake - содержит структуру Recipe
+	// записали значения
 	for _, structRecipe := range recipeOld.Cakes {
 		mapCakesOld[structRecipe.Name] = structRecipe
 	}
 
 	mapCakesNew := make(map[string]dbreader.Recipe)
-
 	for _, structRecipe := range recipeNew.Cakes {
 		mapCakesNew[structRecipe.Name] = structRecipe
 	}
 
-	for nameOld, cakeOld := range mapCakesOld {
-		if cakeNew, exists := mapCakesNew[nameOld]; !exists {
-			fmt.Printf("REMOVED cake %v\n", nameOld)
+	// recipesOld - структура Recipe (старая бд)
+	// recipesNew - структура Recipe (новая бд)
+	for _, recipesOld := range mapCakesOld {
+		//сравниваем поля Name структуры Recipe
+		if recipesNew, exists := mapCakesNew[recipesOld.Name]; !exists {
+			fmt.Printf("REMOVED cake %v\n", recipesOld.Name)
 		} else {
-			if cakeOld.Time != cakeNew.Time {
-				fmt.Printf("CHANGED cooking time for cake %v - %v instead of %v\n", nameOld, cakeNew.Time, cakeOld.Time)
+			// сравниваем поле Time структуры Recipe
+			if recipesOld.Time != recipesNew.Time {
+				fmt.Printf("CHANGED cooking time for cake %v - %v instead of %v\n", recipesOld.Name, recipesNew.Time, recipesOld.Time)
 			}
 
+			// делаем map - ключ - название ингредиента / значение - структура Ingredient
 			mapIngredientsOld := make(map[string]dbreader.Ingredient)
-			for _, ingredientOld := range cakeOld.Ingredients {
+			for _, ingredientOld := range recipesOld.Ingredients {
 				mapIngredientsOld[ingredientOld.Name] = ingredientOld
 			}
 
 			mapIngredientsNew := make(map[string]dbreader.Ingredient)
-			for _, ingredientNew := range cakeOld.Ingredients {
+			for _, ingredientNew := range recipesNew.Ingredients {
 				mapIngredientsNew[ingredientNew.Name] = ingredientNew
 			}
 
-			for ingrNameOld, _ := range mapIngredientsOld {
-				if _, exists := mapIngredientsNew[ingrNameOld]; !exists {
-					fmt.Printf("REMOVED ingredient %v for cake %v\n", ingrNameOld, nameOld)
+			for _, ingridientOld := range mapIngredientsOld {
+				if ingridienNew, exists := mapIngredientsNew[ingridientOld.Name]; !exists {
+					fmt.Printf("REMOVED ingredient %v for cake %v\n", ingridientOld.Name, recipesOld.Name)
+				} else {
+					fmt.Println(ingridienNew)
 				}
 			}
 
@@ -61,7 +66,7 @@ func main() {
 
 	}
 
-	for nameNew, _ := range mapCakesNew {
+	for nameNew := range mapCakesNew {
 		if _, exists := mapCakesOld[nameNew]; !exists {
 			fmt.Printf("ADDED cake %v\n", nameNew)
 		}
