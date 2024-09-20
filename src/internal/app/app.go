f704b861ea53529a2e5b9a0ef82be77487cb4ad2
jenisepl <jenisepl@student.21-school.ru>
2024-09-20 17:41:54 +0300

    change readme struct

--- src/internal/app/app.go
+++ src/internal/app/app.go
@@ -1,10 +1,28 @@
 package app
 
 import (
+	"bufio"
 	"day01/internal/dbcompare"
 	"day01/internal/dbreader"
 	"day01/internal/utils"
+	"fmt"
+	"os"
 )
+
+// RunConvertDB запускает процесс чтения базы данных и преобразования форматов
+func RunReadDB() {
+	// Получаем путь к файлу из аргументов командной строки
+	filepath, err := utils.ParseFileForReadDB()
+	utils.HandleError(err)
+
+	// Читаем файл и записываем данные в структуру
+	recipe, fileType, err := dbreader.ReadDB(filepath)
+	utils.HandleError(err)
+
+	// Преобразуем и выводим данные в противоположном формате
+	err = dbreader.PrettyPainting(recipe, fileType)
+	utils.HandleError(err)
+}
 
 // RunCompareDB запускает процесс чтения и сравнения баз данных
 func RunCompareDB() {
@@ -22,17 +40,38 @@ func RunCompareDB() {
 	dbcompare.CompareDB(recipeOld, recipeNew)
 }
 
-// RunConvertDB запускает процесс чтения базы данных и преобразования форматов
-func RunReadDB() {
-	// Получаем путь к файлу из аргументов командной строки
-	filepath, err := utils.ParseFileForReadDB()
+func RunCompareFS() {
+	filepathForOld, filepathForNew, err := utils.ParseFileForCompareDB()
 	utils.HandleError(err)
 
-	// Читаем файл и записываем данные в структуру
-	recipe, fileType, err := dbreader.ReadDB(filepath)
-	utils.HandleError(err)
+	fileOld, err := os.Open(filepathForOld)
+	if err != nil {
+		fmt.Println("Ошибка открытия файла")
+	}
+	defer fileOld.Close()
 
-	// Преобразуем и выводим данные в противоположном формате
-	err = dbreader.PrettyPainting(recipe, fileType)
-	utils.HandleError(err)
+	data := make(map[string]int)
+	scanner := bufio.NewScanner(fileOld)
+	for scanner.Scan() {
+		txt := scanner.Text()
+		data[txt]++
+	}
+
+	fileNew, err := os.Open(filepathForNew)
+	if err != nil {
+		fmt.Println("Ошибка открытия файла")
+	}
+	defer fileNew.Close()
+
+	scanner = bufio.NewScanner(fileOld)
+	for scanner.Scan() {
+		txt := scanner.Text()
+		for str := range data {
+			if str != txt {
+				fmt.Println("ADDED %q\n", txt)
+			}
+		}
+		data[txt]++
+	}
+
 }
